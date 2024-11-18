91c1cd6d360b4c52a6ff9809591b1cf3cd378dcb
Mostafa Milly <57003058+MostafaKMilly@users.noreply.github.com>
2024-11-18 17:06:10 +0300

    Update left command

--- src/commands/leftCommand.ts
+++ src/commands/leftCommand.ts
@@ -6,7 +6,8 @@ import { DIRECTIONS } from "../constants/directions";
 export class LeftCommand extends RequiresPlacementCommand {
   executeWithPlacedRobot(robot: IPlacedRobot): void {
     const idx = DIRECTIONS.indexOf(robot.direction);
-    robot.changeDirection(DIRECTIONS[(idx + 1) % 4]);
+    const newIdx = (idx - 1 + DIRECTIONS.length) % DIRECTIONS.length;
+    robot.changeDirection(DIRECTIONS[newIdx]);
     Logger.info(`Robot now facing ${robot.direction}`);
   }
 }
